ded37793efe69275558fb901d2f658998d6172cf
NikolasMelui <slak@samaradom.ru>
2018-01-20 15:16:05 +0400

    Add server.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
+const port = 3000;
+
 let app = express();
 
-app.get('/api', () => {});
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'Hello there!'
+  });
+});
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));
